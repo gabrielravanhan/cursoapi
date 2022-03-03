aa75a366fa2b00998b836c11ba8ebbe91045ebb4
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2022-03-03 16:50:46 -0300

    Criei o CursoRepository e configurei o application.properties
    Também alterei o tipo do atributo id de int para Integer no model de Curso

--- cursoapi/src/main/java/br/com/gabriel/cursoapi/model/Curso.java
+++ cursoapi/src/main/java/br/com/gabriel/cursoapi/model/Curso.java
@@ -8,15 +8,15 @@ public class Curso {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
 
     private String nomecurso;
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
