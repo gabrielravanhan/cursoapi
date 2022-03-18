a1806734213b6447536ce898d9ad9d8e3fc1bc7e
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2022-03-18 20:32:13 -0300

    Arrumei a dependência do Flyway e criei a versão 001 no db.migration

---
+++ cursoapi/src/main/resources/db/migration/V001__Criar_Tabela_Curso.sql
@@ -0,0 +1,9 @@
+CREATE TABLE curso (
+    id  INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nomecurso   VARCHAR(150)
+);
+
+INSERT INTO curso(nomecurso)   VALUES('Administração');
+INSERT INTO curso(nomecurso)   VALUES('Contabilidade');
+INSERT INTO curso(nomecurso)   VALUES('Desenvolvimento de Sistemas');
+INSERT INTO curso(nomecurso)   VALUES('Química');
