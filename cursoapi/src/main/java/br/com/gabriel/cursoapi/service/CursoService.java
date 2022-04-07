6fe4c1263b76ba1f7ef92433603974b1696710da
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2022-04-07 20:15:46 -0300

    Criei a classe ObjectNotFoundException e o método para encontrar um curso no CursoService

--- cursoapi/src/main/java/br/com/gabriel/cursoapi/service/CursoService.java
+++ cursoapi/src/main/java/br/com/gabriel/cursoapi/service/CursoService.java
@@ -2,10 +2,12 @@ package br.com.gabriel.cursoapi.service;
 
 import br.com.gabriel.cursoapi.model.Curso;
 import br.com.gabriel.cursoapi.repository.CursoRepository;
+import br.com.gabriel.cursoapi.service.Exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CursoService {
@@ -15,5 +17,14 @@ public class CursoService {
 
     public List<Curso> listarTodosCursos() {
         return cursoRepository.findAll();
+    }
+
+    public Curso findCurso(Integer id) {
+        Optional<Curso> curso = cursoRepository.findById(id);
+
+        return curso.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado!!! id: "
+                        + id + " Tipo: " + Curso.class.getName()
+        ));
     }
 }
