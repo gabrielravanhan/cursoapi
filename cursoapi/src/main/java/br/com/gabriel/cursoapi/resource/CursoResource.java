fa1e975261f1b99b2750b0369680ae480aec9e24
Gabriel José Soto Ravanhan <gabrielravanhanjose@gmail.com>
2022-05-05 11:43:46 -0300

    Criei o método para buscar um curso pelo seu id

--- cursoapi/src/main/java/br/com/gabriel/cursoapi/resource/CursoResource.java
+++ cursoapi/src/main/java/br/com/gabriel/cursoapi/resource/CursoResource.java
@@ -1,13 +1,17 @@
 package br.com.gabriel.cursoapi.resource;
 
 import br.com.gabriel.cursoapi.model.Curso;
+import br.com.gabriel.cursoapi.repository.CursoRepository;
 import br.com.gabriel.cursoapi.service.CursoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cursos")
@@ -16,8 +20,17 @@ public class CursoResource {
     @Autowired
     private CursoService cursoService;
 
+    @Autowired
+    private CursoRepository cursoRepository;
+
     @GetMapping("/todos")
     public List<Curso> listarTodosCursos() {
         return cursoService.listarTodosCursos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Curso> buscarPeloId(@PathVariable Integer id) {
+        Optional<Curso> curso = cursoRepository.findById(id);
+        return curso.isPresent() ? ResponseEntity.ok(curso.get()) : ResponseEntity.notFound().build();
     }
 }
