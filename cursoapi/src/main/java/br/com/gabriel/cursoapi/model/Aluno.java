f9141b1fcbb3b822fc82bbf2baf558df62555270
Gabriel José Soto Ravanhan <gabrielravanhanjose@gmail.com>
2022-06-02 11:39:18 -0300

    Criação da tabela de alunos

---
+++ cursoapi/src/main/java/br/com/gabriel/cursoapi/model/Aluno.java
@@ -0,0 +1,9 @@
+package br.com.gabriel.cursoapi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "aluno")
+public class Aluno {
+}
---
+++ cursoapi/src/main/resources/db/migration/V002__Criar_Tabela_Aluno.sql
@@ -0,0 +1,5 @@
+CREATE TABLE aluno (
+    id BIGINT   NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nomealuno   VARCHAR(150),
+    curso_id    INT NOT NULL
+);
