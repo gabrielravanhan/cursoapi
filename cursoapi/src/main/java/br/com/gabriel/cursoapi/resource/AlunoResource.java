895499a5bd00e18f5dadebc9e38afe80f3e1212c
Gabriel José Soto Ravanhan <gabrielravanhanjose@gmail.com>
2022-06-23 11:34:14 -0300

    Listar Alunos

--- cursoapi/src/main/java/br/com/gabriel/cursoapi/resource/AlunoResource.java
+++ cursoapi/src/main/java/br/com/gabriel/cursoapi/resource/AlunoResource.java
@@ -3,6 +3,8 @@ package br.com.gabriel.cursoapi.resource;
 import br.com.gabriel.cursoapi.model.Aluno;
 import br.com.gabriel.cursoapi.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +17,8 @@ public class AlunoResource {
     @Autowired
     private AlunoRepository alunoRepository;
 
+    @GetMapping("/todos")
     public List<Aluno> listarTodosAlunos() {
-        return alunoRepository.findAll();
+        return alunoRepository.findAll(Sort.by("nomealuno").descending());
     }
 }
